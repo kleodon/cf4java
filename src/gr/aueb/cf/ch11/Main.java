39aec55353d083ecc2563f70c015778bdae24b4e
nikolaosRigas <nicolaos.rigas@gmail.com>
2023-09-27 12:34:17 +0300

    Classes User, UserCredentials, Main creation

---
+++ src/gr/aueb/cf/ch11/Main.java
@@ -0,0 +1,25 @@
+package gr.aueb.cf.ch11;
+import gr.aueb.cf.ch11.model.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        long nickID = 1;
+        User Nick = new User(nickID,"Nick","Rigas");
+        UserCredentials NickCredentials = new UserCredentials(nickID,"nicolaosrigas","12345");
+
+        System.out.println( "User {" +
+                            "ID: " + Nick.getId() + "," +
+                            "First Name: " + Nick.getFirstName()  + "," +
+                            "Last Name: " + Nick.getLastName() + "," + "}"
+        );
+
+        System.out.println( "User Credentials {" +
+                            "ID: " + NickCredentials.getId() + "," +
+                            "User Name: " + NickCredentials.getUserName()  + "," +
+                            "Password: " + NickCredentials.getPassword() + "," + "}"
+        );
+
+
+    }
+}
